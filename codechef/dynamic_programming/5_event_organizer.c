87aae062224377ab5386aa82275a0578e3dedf14
tclack88 <tclack@aol.com>
2025-09-12 15:05:00 -0700

    finish even organizer, a weighted interval scheduling algorithm. Key changes: binary-search-strategized 'prev' array finder and an 'opt' array, each described at top

--- codechef/dynamic_programming/5_event_organizer.c
+++ codechef/dynamic_programming/5_event_organizer.c
@@ -1,5 +1,21 @@
-// Weighted Interval Scheduling Algorithm. Solution strategy described here:
-// https://www.youtube.com/watch?v=iIX1YvbLbvc
+// Weighted Interval Scheduling Algorithm. Solution strategy described here:  https://www.youtube.com/watch?v=iIX1YvbLbvc
+/* Strategy: Read in start, end and compensation into a Task struct then qsort by end. Then create a "prev" array which holds the index of the previous index that ends at or before this task starts. The reason for this is we want to add any previous values that exist and end at or before the next task starts. Then we make an "opt" (for optimum) array of the same size where we hold the values for the best/optimal value up to and/or including the task we are looking at. It's calculated by adding the weight of the present tasks compensation to the prev at that the prev index or ignoring the new weight and just consider the last opt value, whichever is higher:
+(opt[j] = max(w_j + opt[prev[j]], opt[j-1] )
+INPUT:		OUTPUT:
+2
+4
+1 2 100
+2 3 200
+3 4 1600
+1 3 2100			3700
+3
+1 10 2000
+2 5 100
+6 9 400				2000
+To make the above more comprehensible, here's the "prev" and "opt" for the first example (set 0 to first index of each as base case where "nothing" is included):
+prev: 0   0   1   0    3
+opt:  0   100 300 2100 3700
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,15 +44,27 @@ int comp(const void* a, const void* b){
 	return taskA->e - taskB->e;
 }
 
-void print_task(struct Task* task,int i){
-	printf("task %d:\t%d %d %d\n", i+1, task->s, task->e, task->c);
-}
-
-void print_tasklist(struct Task* tasklist[], int size){
-    for (int i=0; i<size; i++){
+void bin_search_prev(struct Task* tasklist[], int prev[], int size){
+	for (int i=size; i > 0; i--){
 		struct Task* task = tasklist[i];
-		print_task(task,i);
-    }
+		int start = task->s;
+		int top = i;
+		int bottom = 0;
+		int best = 0;
+		int mid = (top + bottom)/2;
+		while( bottom <= top ){
+			if (tasklist[mid]->e <= start) // drive upward if
+				bottom = mid;	// multiple of target val (start)
+			else if (tasklist[mid]->e > start)
+				top = mid;
+			best = bottom;
+			mid = (top + bottom)/2;
+			if (bottom == mid) // avoid infinite loops
+				break;
+		}
+		prev[i] = best;
+	}
+	prev[0] = 0;
 }
 
 struct Task* create_task(int s, int e, int c){
@@ -53,30 +81,23 @@ int main() {
 	while (T--){
 		int best = 0;
 	    scanf("%d", &size);
-		struct Task* tasklist[size];
-	    for (int i=0; i<size; i++){
+		struct Task* tasklist[size+1];
+		struct Task* task = create_task(0, 0, 0);
+        tasklist[0] = task;
+	    for (int i=1; i<=size; i++){
 	        scanf("%d %d %d", &s, &e, &c);
             struct Task* task = create_task(s, e, c);
             tasklist[i] = task;
 	    }
-	    //print_tasklist(tasklist, size);
-	    //printf("\nsorted:\n");
-		qsort(tasklist, size, sizeof(struct Task*), comp);
-	    //print_tasklist(tasklist, size);
-	    //printf("\n");
-		int sched[50] = {0};
-		//memset(sched,0,size*sizeof(int));
-		for(int i=0; i<size; i++){
-			struct Task* task = tasklist[i];
-			//print_task(task,i);
-			int start = task->s;
-			int end = task->e;
-			sched[end] = max(sched[end],sched[start]+task->c);
-			best = max(best, sched[end]);
-		}
-		//for (int i=0; i<49; i++)
-		//	printf("%d, ",sched[i]);
-		printf("%d\n",best);
+		qsort(tasklist, size+1, sizeof(struct Task*), comp);
+		int prev[size+1]; // prev = "previous" as in the latest task that ends at or just before the current task (in task index, counting from 1) begins
+		bin_search_prev(tasklist, prev, size);
+		int opt[size+1]; // opt = "optimal"
+		opt[0] = 0;
+		for (int i=1; i<=size; i++)
+			opt[i] = max(tasklist[i]->c + opt[prev[i]], opt[i-1]);
+		
+		printf("%d\n",opt[size]);
 	}
     return 0;
 }
